e16c0903378c4d4e40783953655e563fb83cbd7f
David Mateo <david.mateo.valderrama@gmail.com>
2016-06-09 14:29:58 +0800

    Labyrinth demo: academic simulation
    Changes in labyrinth to make it easier to
    use as academic example of swarm robotics.

--- examples/labyrinth/labyrinth.py
+++ examples/labyrinth/labyrinth.py
@@ -1,14 +1,56 @@
-from marabunta import MockBody, MockNetwork
-from marabunta.models import AreaCoverageRobot
+from marabunta import BaseRobot, MockBody, MockNetwork
 import random
 
 # for visualization
 import numpy as np
 import pylab as pl
 
+class myRobot(BaseRobot):
+    def __init__(self, setting):
+        body = MockBody(setting.get("position") ,setting.get("heading"))
+        network = MockNetwork(setting.get("ID"))
+        BaseRobot.__init__(self,body, network)
+        return
 
-class myRobot(AreaCoverageRobot):
-    pass
+    def spread_target(self):
+        neis = self.get_agents().values()
+        pos = self.body.get_position()
+        # Get both neighbors and obstacles, in relative coordinates
+        obstacles = self.body.obstacle_coordinates()
+        points = [ [nei[0]-pos[0], nei[1]-pos[1]] for nei in neis] + obstacles
+        if points:
+            target = [0.,0.]
+            for p in points:
+                d2 = p[0]**2 + p[1]**2
+                weight = (1.0/d2 )**1.5
+                if d2>0:
+                    target[0] -= p[0]*weight
+                    target[1] -= p[1]*weight
+        else:
+            target= None
+        return target
+
+    def move_towards_target(self, target, deltat, v):
+        threshold = 5
+        if target[0]**2 + target[1]**2 > threshold*threshold:
+            self.align(target)
+            self.move_forward(deltat, v)
+        else:
+            self.move_forward(deltat, 0)
+        return
+
+    def update(self, deltat, v):
+        self.broadcast_state()
+        # Perform swarming
+        target = self.spread_target()
+        if not target:
+            h= self.body.get_heading()
+            target = [10.*cos(h) ,10.*sin(h)]
+        # Avoid obstacles
+        target = self.correct_target(target)
+        self.move_towards_target(target, deltat, v)
+        return
+
 
 num_robots = 50
 dt=0.5
@@ -44,7 +86,8 @@ def plot(robots, obstacles):
 #-----------------------------------------------------
 
 settings = [ {"ID":"R%02i"%i, "position":random_position(1.5,0.5,0.2), "heading":0.} for i in range(num_robots)]
-robots=[ myRobot( MockBody(s.get("position") ,s.get("heading")), MockNetwork(s.get("ID")) , 5.) for s in settings]
+
+robots = [ myRobot(s) for s in settings]
 
 [robot.body.load_obstacles(map_file) for robot in robots]
 [robot.turn_on() for robot in robots]
